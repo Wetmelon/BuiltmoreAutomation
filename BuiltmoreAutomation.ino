c6036462426574f2ce94b777cb166dfcd55f0e29
Wetmelon <pfguenette@gmail.com>
2016-12-08 22:36:32 -0500

    Finished Code Rev 1 (State Machine, Seeder on delay)

--- BuiltmoreAutomation.ino
+++ BuiltmoreAutomation.ino
@@ -1,23 +1,35 @@
+
 #include "Builtmore_Defines.h"
-#include <DAC_MCP49xx.h>
+#include <MultiStepper.h>
 #include <AccelStepper.h>
+#include <DAC_MCP49xx.h>
 
 typedef enum {
     BA_STATE_IDLE = 0,
     BA_STATE_ERROR,
     BA_STATE_INIT,
-    BA_STATE_WAITING,
+    BA_STATE_SWITCH1,
+    BA_STATE_SWITCH2,
+    BA_STATE_STAMPER_ONLY,
+    BA_STATE_BOTH,
+    BA_STATE_REVERSE,
 } ba_state_t;
 
-AccelStepper stepper(1, STEP1_PIN, DIR1_PIN);
-DAC_MCP49xx myDac(DAC_MCP49xx::MCP4921, CS_PIN);
-ba_state_t eState;
+AccelStepper stamperStepper(AccelStepper::DRIVER, STEP1_PIN, DIR1_PIN);
+AccelStepper seederStepper(AccelStepper::DRIVER, STEP2_PIN, DIR2_PIN);
 
-uint32_t timer = 0;
+DAC_MCP49xx myDac(DAC_MCP49xx::MCP4921, CS_PIN);
+ba_state_t state;
+
+uint32_t seederTimer = 0;
+uint32_t speedTimer = 0;
+float traySpeed = 0;
+
+uint32_t move = 0;
+uint32_t accel = 1;
 
 void setup(){
-    
-    eState = BA_STATE_INIT;
+    state = BA_STATE_INIT;
 
     // Prep uC Pins
     pinMode(EN1_PIN, OUTPUT);
@@ -47,6 +59,9 @@ void setup(){
 
     digitalWriteFast(CS_PIN, HIGH);
 
+    attachInterrupt(digitalPinToInterrupt(14), speedSwitchOneHandler, FALLING);
+    attachInterrupt(digitalPinToInterrupt(15), speedSwitchTwoHandler, FALLING);
+
     Serial.begin(250000);
     delay(1000);
     Serial.println("Serial Starting.");
@@ -54,65 +69,93 @@ void setup(){
     analogReadResolution(12);
     analogReadAveraging(4);
 
-    myDac.output(0);
-    
-    stepper.setMaxSpeed(2000);
-    stepper.setSpeed(500);
+    myDac.output(DESIRED_SPEED);
+
+    state = BA_STATE_IDLE;
 }
 
 void loop(){
 
-    positionControl();
-    //speedControl();
+    switch (state){
+    case BA_STATE_STAMPER_ONLY:
+        if (millis() - seederTimer > SEEDER_DELAY){
+            seederStepper.setSpeed(traySpeed / DISC_RADIUS_M * 100 / (PI)*  MICROSTEPPING);
+            state = BA_STATE_BOTH;
+        }
+            stamperStepper.run();
+        break;
 
+    case BA_STATE_BOTH:
+        if (stamperStepper.distanceToGo() == 0){
+            stamperStepper.moveTo(0);
+            state = BA_STATE_REVERSE;
+        }
+        else {
+            seederStepper.runSpeed();
+            stamperStepper.run();
+        }
+        break;
+
+    case BA_STATE_REVERSE:
+        if (stamperStepper.distanceToGo() == 0){
+            seederStepper.setSpeed(0);
+            state = BA_STATE_IDLE;
+        }
+        else {
+            seederStepper.runSpeed();
+            stamperStepper.run();
+        }
+        break;
+
+    case BA_STATE_INIT:     setup(); break;
+    case BA_STATE_SWITCH2:  startRunning(); break;
+    case BA_STATE_SWITCH1:
+    case BA_STATE_IDLE:
+    case BA_STATE_ERROR:
+    default: break;
+    }
 }
 
-void testDAC(){
-    static uint16_t dacVal = 0;
-    myDac.output(dacVal++);
-    Serial.print("DacVal: ");
-    Serial.print(dacVal);
-    Serial.print("\tAnalog: ");
-    Serial.println(analogRead(A4));
-    if (dacVal >= 4095)
-        dacVal = 0;
-}
-
-void speedControl(){
-    stepper.runSpeed();
-}
-
-void positionControl(){
-    if (stepper.distanceToGo() == 0)
-    {
-        // Random change to speed, position and acceleration
-        // Make sure we dont get 0 speed or accelerations
+void startRunning(){
+    if (state == BA_STATE_SWITCH2){
+#ifdef DEBUG
         Serial.print("Timer: ");
         Serial.println(millis() - timer);
-        delay(1000);
-        int move = rand() % 200;
-        int speed = rand() % 200;
-        int accel = rand() % 200;
+#endif
 
-        move = 20000;
-        speed = 1000;
-        accel = 500;
+        delay(STAMPER_DELAY);
 
-        stepper.move(move);
-        stepper.setMaxSpeed(speed);
-        stepper.setAcceleration(accel);
+        stamperStepper.setMaxSpeed(traySpeed / DISC_RADIUS_M * 100 / (PI)*  MICROSTEPPING);
+        stamperStepper.setAcceleration(STEPPER_ACCEL * MICROSTEPPING);
+        stamperStepper.move(FORWARD_STEPS * MICROSTEPPING);
 
+        state = BA_STATE_STAMPER_ONLY;
+
+#ifdef DEBUG
         Serial.print("Move: ");
-        Serial.print(stepper.targetPosition());
+        Serial.print(stamperStepper.targetPosition());
         Serial.print("\tDiff: ");
-        Serial.print(stepper.distanceToGo());
+        Serial.print(stamperStepper.distanceToGo());
         Serial.print("\tSpeed: ");
-        Serial.print(stepper.maxSpeed());
+        Serial.print(stamperStepper.maxSpeed());
         Serial.print("\tAccel: ");
-        Serial.println(accel);
+        Serial.println(STEPPER_ACCEL);
+
         timer = millis();
+#endif
     }
-    
-    stepper.run();
-    //delayMicroseconds(1);
+}
+
+void speedSwitchOneHandler(){
+    if (state == BA_STATE_IDLE){
+        speedTimer = millis();
+        state = BA_STATE_SWITCH1;
+    }
+}
+
+void speedSwitchTwoHandler(){
+    if (state == BA_STATE_SWITCH1){
+        traySpeed = SWITCH_DISTANCE_M / (millis() - speedTimer);
+        state = BA_STATE_SWITCH2;
+    }
 }
--- Builtmore_Defines.h
+++ Builtmore_Defines.h
@@ -10,21 +10,34 @@
 #endif
 
 
-#define EN1_PIN     (0)
-#define STEP1_PIN   (1)
-#define DIR1_PIN    (2)
-#define ENC_A_PIN   (3)
-#define ENC_B_PIN   (4)
-#define ENC_Z_PIN   (5)
-#define EN2_PIN     (6)
-#define STEP2_PIN   (7)
-#define DIR2_PIN    (8)
-#define LIMIT1_PIN  (14)
-#define LIMIT2_PIN  (15)
-#define LIMIT3_PIN  (16)
-#define LIMIT4_PIN  (17)
-#define CS_PIN      (10)
+#define EN1_PIN     		(0)
+#define STEP1_PIN   		(1)
+#define DIR1_PIN    		(2)
+#define ENC_A_PIN   		(3)
+#define ENC_B_PIN   		(4)
+#define ENC_Z_PIN   		(5)
+#define EN2_PIN     		(6)
+#define STEP2_PIN   		(7)
+#define DIR2_PIN    		(8)
+#define LIMIT1_PIN  		(14)
+#define LIMIT2_PIN  		(15)
+#define LIMIT3_PIN  		(16)
+#define LIMIT4_PIN  		(17)
+#define CS_PIN      		(10)
 
+#define DESIRED_SPEED       (4095/2)
+#define SWITCH_DISTANCE_M 	(.05)
+#define DISC_RADIUS_M       (.09144)
+
+#define FORWARD_STEPS       (2000)
+#define REVERSE_STEPS       (500)
+#define STEPPER_ACCEL       (1000)
+#define MICROSTEPPING       (1)
+
+#define STAMPER_DELAY       (1000)
+#define SEEDER_DELAY        (1000)
+
+//#define DEBUG
 
 #endif
 
